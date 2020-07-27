8c2bd82536e9c50ea4b7359acf15ce1e98535a65
Moises David Caicedo Corena <moisescaicedo15@gmail.com>
2020-07-27 11:12:47 -0500

    IonAlert

--- src/app/pages/alert/alert.page.ts
+++ src/app/pages/alert/alert.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-alert',
@@ -6,10 +7,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert.page.scss'],
 })
 export class AlertPage implements OnInit {
+ titulo="Alerta"
+  constructor(private alertCtr:AlertController) {     }
 
-  constructor() { }
 
-  ngOnInit() {
+  async presentarAlerta(){
+    const alerta = await this.alertCtr.create({
+      header:'Alerta Header mm',
+      subHeader:'Alerta SubHeader',
+      message:'Alerta Mensaje',
+      buttons:[
+        {text:'ok',handler: ()=>{console.log("ok")}},
+        {text:'Cancelar',role: "cancel", handler: ()=>{console.log("Cancelar")}},
+      ]
+    }) ;
+    await alerta.present();
   }
 
+  async presentarAlertaPrompt(){
+      const alerta = await this.alertCtr.create({
+        header:"Escriba el nuebo titulo",
+        inputs:[{type:"text",placeholder:"Nuevo titulo",label:"Digite el nuevo titulo",name:"nuevoTitulo"}],
+        buttons:[
+          {
+            text:"ok",
+            role:"ok",
+            handler:(e)=> { 
+            //  console.log(e.nuevoTitulo )
+            this.titulo= e.nuevoTitulo
+              }
+          },
+          {
+            text:"Cancelar",
+            role:'cancel',
+            handler:()=>{console.log("Se ha cancelado")}
+          }
+        ]
+      })
+      await alerta.present();
+  }
+
+
+  ngOnInit() {
+    
+  }
+
+}
+
+
+interface Alertas {
+  nombre:string,
+  typo:string
 }
